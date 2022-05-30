58589b140e915b5660be4277abca2bb67aeac621
Sebastian <sebastian.rob.cam@gmail.com>
2022-05-30 07:05:58 -0500

    Optimi JS.js and fix html's

--- JS/JS.js
+++ JS/JS.js
@@ -1,67 +1,26 @@
 // (--->    HOME indexPage   <---)
     // (--->    top menu left-part   <---)
-    let sbButton = document.getElementById("sbButton");
-    let wkButton = document.getElementById("wkButton");
-    let abButton = document.getElementById("abButton");
-    let ctbutton = document.getElementById("ctButton");
-    
-    sbButton.onclick = function(){
-        location.href="/index.html"
+    function sbButton() {
+        window.location.href="/index.html";
     };
-    wkButton.onclick = function(){
-        location.href="/work.html";
+    function wkButton() {
+        window.location.href="/work.html";
     };
-    abButton.onclick = function(){
-        location.href="/about.html";
+    function abButton() {
+        window.location.href="/about.html";
     };
-    ctButton.onclick = function(){
-        location.href="/contact.html";
-    };
-
-    let sbButtonFooter = document.getElementById("sbButtonFooter");
-    let wkButtonFooter = document.getElementById("wkButtonFooter");
-    let abButtonFooter = document.getElementById("abButtonFooter");
-    let ctButtonFooter = document.getElementById("ctButtonFooter");
-    
-    sbButtonFooter.onclick = function(){
-        location.href="/index.html";
-    };
-    wkButtonFooter.onclick = function(){
-        location.href="/work.html";
-    };
-    abButtonFooter.onclick = function(){
-        location.href="/about.html";
-    };
-    ctButtonFooter.onclick =function(){
-        location.href="/contact.html";
+    function ctButton() {
+        window.location.href="/contact.html";
     };
     // (--->    top menu center-part   <---)
-    let igButton = document.getElementById("igButton");
-    let gitButton = document.getElementById("gitButton");
-    let twButton = document.getElementById("twButton");
-
-    igButton.onclick = function(){
-        location.href="https://www.instagram.com/_robl3s_/?hl=en";
+    function igButton() {
+        window.location.href="https://www.instagram.com/_robl3s_/?hl=en";
     };
-    gitButton.onclick = function(){
-        location.href="https://github.com/neu7ral";
+    function gitButton() {
+        window.location.href="https://github.com/neu7ral";
     };
-    twButton.onclick = function(){
-        location.href="https://twitter.com/RocsSebastian";
-    };
-
-    let igButtonFooter = document.getElementById("igButtonFooter");
-    let gitButtonFooter = document.getElementById("gitButtonFooter");
-    let twButtonFooter = document.getElementById("twButtonFooter");
-
-    igButtonFooter.onclick = function(){
-        location.href="https://www.instagram.com/_robl3s_/?hl=en";
-    };
-    gitButtonFooter.onclick = function(){
-        location.href="https://github.com/neu7ral";
-    };
-    twButtonFooter.onclick = function(){
-        location.href="https://twitter.com/RocsSebastian";
+    function twButton() {
+        window.location.href="https://twitter.com/RocsSebastian";
     };
     // (--->    top menu right-part   <---)
     let menuRightDate =()=>{
@@ -83,21 +42,15 @@
 
     };
     menuRightDate();
-
 // (--->    END indexPage   <---)
-
 // (--->    Work workPage   <---)
-let jsButtonWork = document.getElementById("jsWork");
-let piButtonWork = document.getElementById("pyWork");
-let iaButtonWork = document.getElementById("iaWork");
-
-jsButtonWork.onclick =function(){
-    location.href="/html/jsProjects.html";
-}
-piButtonWork.onclick = function(){
-    location.href="/html/pyProjects.html";
-};
-iaButtonWork.onclick = function(){
-    location.href="/html/iaProjects.html";
-};
+    function jsProjects() {
+        window.location.href="/html/jsProjects.html";
+    };
+    function pyProjects() {
+        window.location.href="/html/pyProjects.html";
+    };
+    function iaProjects() {
+        window.location.href="/html/iaProjects.html";
+    };
 // (--->    Work workPage   <---)
